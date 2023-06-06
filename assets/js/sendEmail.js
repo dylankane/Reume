073a053b3ea0292b64385b6c2868bfae515bfa53
Dylan <dylankane3@gmail.com>
2023-06-06 12:01:32 +0000

    Implement email intergration on contact page

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,11 @@
+ // var templateParams = {
+            //     name: 'Dylan',
+            //     notes: 'This is the project you want..'
+            // };
+
+            // emailjs.send('service_hnu0uwq', 'RosieTemplate1', templateParams)
+            //     .then(function (response) {
+            //         console.log('SUCCESS!', response.status, response.text);
+            //     }, function (error) {
+            //         console.log('FAILED...', error);
+            //     });
