8d411ac5256d6e719d2d84400dd53b4869514cc7
Dylan <dylankane3@gmail.com>
2023-06-06 13:04:29 +0000

    Fix bugs with sendMail function in sendEmail.js

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,11 +1,13 @@
- // var templateParams = {
-            //     name: 'Dylan',
-            //     notes: 'This is the project you want..'
-            // };
-
-            // emailjs.send('service_hnu0uwq', 'RosieTemplate1', templateParams)
-            //     .then(function (response) {
-            //         console.log('SUCCESS!', response.status, response.text);
-            //     }, function (error) {
-            //         console.log('FAILED...', error);
-            //     });
+ function sendMail(contactForm) {
+     var templateParams = {
+         "from_name": contactForm.fullname.value,
+         "from_email": contactForm.emailaddress.value,
+         "project_request": contactForm.projectsummary.value
+     };
+     emailjs.send('service_hnu0uwq', 'RosieTemplate1', templateParams)
+         .then(function (response) {
+             console.log('SUCCESS!', response.status, response.text);
+         }, function (error) {
+             console.log('FAILED...', error);
+         });
+ }
